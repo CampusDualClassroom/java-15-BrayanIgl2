f8c2ca8838a9d03f3fe90fbfe48e64324c3510df
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-26 14:33:21 +0200

    Ej 15 OK

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,36 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+
+    //Formatea la fecha a String
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(date);
+    }
+    @Override
+    public Object getSpecificData() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Localización: ").append(getLocation()).append("\n");
+        builder.append("Caducidad: ").append(getFormattedDate(expirationDate));
+        return builder.toString();
+    }
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
+
